5df4404326b689b802a73a380b74b2c248c1d3fd
Yurkiv Misha <jurkiw.misha@gmail.com>
2017-09-07 20:53:39 +0300

    Add resorts parser

--- parser.rb
+++ parser.rb
@@ -1,3 +1,20 @@
-tmp = open('resorts.html')
-page = Nokogiri::HTML(tmp)
-resorts = page.css("li.menu__item").map{|mi| {resort: mi.children.css("a").first.text.downcase.tr(" ", "_"), cams:mi.css("li.submenu__item a").map{|a| {id: a['href'][/([^\/]+)$/], link: a['href'], title: a.text}}}}
+require 'nokogiri'
+require 'open-uri'
+require 'json'
+
+page = Nokogiri::HTML(open('https://snig.info/en'))
+resorts = page.css('li.menu__item').map do |mi|
+  { resort: mi.children.css('a').first.text.downcase.tr(' ', '_'),
+    cams: mi.css('li.submenu__item a').map do |a|
+      {
+        id: a['href'][/([^\/]+)$/],
+        title: a.text
+      }
+    end
+  }
+end
+resorts.shift
+
+File.open('resorts.json', 'w') do |f|
+  f.write(JSON.pretty_generate(resorts))
+end
