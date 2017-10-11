88b4b691803275c93d42dc85f8fee1abef0653c5
Yurkiv Misha <jurkiw.misha@gmail.com>
2017-10-11 19:57:23 +0300

    add errors handler

--- snig_bot.rb
+++ snig_bot.rb
@@ -7,24 +7,28 @@ Dotenv.load
 token = ENV['TELEGRAM_API_TOKEN']
 
 Telegram::Bot::Client.run(token) do |bot|
-  resorts = JSON.parse(File.read('resorts.json'), symbolize_names: true)
-  resorts_list = resorts.map { |resort| resort[:resort].prepend('/') }.join("\n")
+  begin
+    resorts = JSON.parse(File.read('resorts.json'), symbolize_names: true)
+    resorts_list = resorts.map { |resort| resort[:resort].prepend('/') }.join("\n")
 
-  bot.listen do |message|
-    case message.text
-    when '/start', '/list'
-      bot.api.sendMessage(chat_id: message.chat.id, text: resorts_list)
-    end
-
-    resort = resorts.find { |r| r[:resort] == message.text }
-    if resort
-      resort[:cams].each do |cam|
-        photo = Faraday::UploadIO.new(open("https://img.snig.info/#{cam[:id]}/last.jpg"), 'image/jpeg')
-        text = "#{cam[:title]}: https://snig.info/#{cam[:id]}"
-
-        bot.api.send_photo(chat_id: message.chat.id, photo: photo, caption: text)
+    bot.listen do |message|
+      case message.text
+      when '/start', '/list'
+        bot.api.sendMessage(chat_id: message.chat.id, text: resorts_list)
       end
-      bot.api.sendMessage(chat_id: message.chat.id, text: '/list')
+
+      resort = resorts.find { |r| r[:resort] == message.text }
+      if resort
+        resort[:cams].each do |cam|
+          photo = Faraday::UploadIO.new(open("https://img.snig.info/#{cam[:id]}/last.jpg"), 'image/jpeg')
+          text = "#{cam[:title]}: https://snig.info/#{cam[:id]}"
+
+          bot.api.send_photo(chat_id: message.chat.id, photo: photo, caption: text)
+        end
+        bot.api.sendMessage(chat_id: message.chat.id, text: '/list')
+      end
     end
+  rescue Telegram::Bot::Exceptions::ResponseError => e
+    retry
   end
 end
